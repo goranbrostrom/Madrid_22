f3ba3b8490b6d863202d43699ae764eb94bc5d6b
Göran Broström <goran.brostrom@umu.se>
2022-01-25 00:49:22 +0100

    add parameter 'linesep'

--- R/tbl.R
+++ R/tbl.R
@@ -1,7 +1,8 @@
-tbl <- function(tt, caption = "", fs = 11, rownames = FALSE, align = NULL){
+tbl <- function(tt, caption = "", fs = 11, rownames = FALSE, align = NULL,
+                linesep = c("", "", "", "", "\\addlinespace")){
     library(kableExtra)
     kbl(tt, caption = caption, booktabs = TRUE, row.names = rownames, 
-        format = "latex", align = align) %>%
+        format = "latex", align = align, linesep = linesep) %>%
         row_spec(row = 0, bold = TRUE) %>%
         kable_styling(full_width = FALSE, font_size = fs,
                     position = "center")
